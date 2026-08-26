a983fb41bb6c77af315f47073eea237bd4b5435e
harrysong306 <harrysong2457@gmail.com>
2026-08-26 22:12:29 +0800

    nest Game class
    Removed phase and currentGridIndex properties from GameState class. Added game property of type Game.

--- backend/src/rooms/schema/GameState.ts
+++ backend/src/rooms/schema/GameState.ts
@@ -1,4 +1,5 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Game } from "../../game/Game.js";
 
 export class Player extends Schema {
   @type("string") name: string = "";
@@ -10,10 +11,5 @@ export class GameState extends Schema {
   @type({ map: Player }) players = new MapSchema<Player>();
   @type("string") gameMode: string = "cooperative"; // Should have this be editable, with some validation after selection from front end menu dropdown.
 
-  // phase of the room, changes when BE-8 (start_game) runs
-  // values: "lobby" | "drawing" | "recall" | "end"
-  @type("string") phase: string = "lobby";
-
-  // which cell of the 5x5 grid we're on (0 to 24)
-  @type("number") currentGridIndex: number = 0;
+  @type(Game) game = new Game();
 }
